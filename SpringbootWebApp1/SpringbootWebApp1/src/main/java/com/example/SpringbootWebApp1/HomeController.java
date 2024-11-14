a90f7a3ff8ce34cc9deb41d605f3e7c9efebb44c
tanvipandhre <37935899+tanvipandhre@users.noreply.github.com>
2024-11-14 14:33:53 -0500

    Sending page through controller

---
+++ SpringbootWebApp1/SpringbootWebApp1/src/main/java/com/example/SpringbootWebApp1/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootWebApp1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        System.out.println("home method called");
+        return "index.jsp";
+    }
+}
---
+++ SpringbootWebApp1/SpringbootWebApp1/src/main/java/com/example/SpringbootWebApp1/SpringbootWebApp1Application.java
@@ -0,0 +1,15 @@
+package com.example.SpringbootWebApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebApp1Application {
+
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootWebApp1Application.class, args);
+	}
+
+}
